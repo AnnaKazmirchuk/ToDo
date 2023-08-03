fe750ec90e69b5714f6dd6657270139fe56e81ef
Anna Kazmirchuk <anyuta32@mail.ru>
2023-08-03 20:52:30 +0300

    Classes added

---
+++ src/main/java/ru/netology/Epic.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+public class Epic extends Task {
+
+    protected String[] subtasks;
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+
+}
---
+++ src/main/java/ru/netology/Task.java
@@ -0,0 +1,12 @@
+package ru.netology;
+public class Task {
+    protected int id;
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
